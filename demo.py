d593dbac3224275c5b1e6ab17752a4d613f8b1bb
hb1105054657 <1105054657@qq.com>
2020-07-29 22:51:23 +0800

    22

---
+++ demo.py
@@ -0,0 +1,20 @@
+# print("好好学习吧！")
+# print(2333)
+# print(2.3333)
+# print(True,False)
+# print(())
+# print([])
+# print({})
+# print(None)
+
+# print("输出输入的数据：" ,input("请输入："))
+
+# name = input("请输入：")
+# print("输出：", name)
+
+# int1 = input("请输入第一个数字：")
+# int2 = input("请输入第二个数字：")
+# print(int(int1) + int(int2))
+# print(len("哈哈哈哈哈哈"))
+
+print("本次共输入", len(input("请输入：")) , "个字")
